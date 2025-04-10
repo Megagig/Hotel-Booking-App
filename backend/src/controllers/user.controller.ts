a37aa8ec14719abab14f16a7332fd5a14fef99f2
megagig <megagigsoftwaresolutions@gmail.com>
2025-04-10 17:51:04 +0100

    Refactor the registerUser controller function

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -2,33 +2,34 @@ import { Request, Response } from 'express';
 import User from '../models/user.model';
 import jwt from 'jsonwebtoken';
 
-export const registerUser = async (
-  req: Request,
-  res: Response
-): Promise<void> => {
+//Register a new user
+export async function registerUser(req: Request, res: Response) {
   try {
-    // Check if user already exists
-    const existingUser = await User.findOne({ email: req.body.email });
+    //destructuring the request body
+    const { email, password, firstName, lastName } = req.body;
 
+    //check if user already exists
+    const existingUser = await User.findOne({ email: req.body.email });
     if (existingUser) {
       res.status(400).json({
         message: 'User already exists',
       });
     }
 
-    // Create a new user
-    const user = new User(req.body);
-
-    // Save the user to database
-    await user.save();
+    // Create a new user instance and save it to the database
+    const newUser = await User.create({
+      email,
+      password,
+      firstName,
+      lastName,
+    });
 
     // Generate JWT token
     const token = jwt.sign(
-      { userId: user._id },
+      { userId: newUser._id },
       process.env.JWT_SECRET_KEY as string,
       { expiresIn: '1d' }
     );
-
     // Set token as HTTP-only cookie
     res.cookie('auth_token', token, {
       httpOnly: true,
@@ -36,11 +37,12 @@ export const registerUser = async (
       maxAge: 86400000, // 1 day in milliseconds
     });
 
-    // Return success response
-    res.status(200).json({ userId: user._id });
+    //return the User
+    res.status(201).json({ user: newUser });
   } catch (error) {
+    console.error('Error registering user:', error);
     res.status(500).json({
       message: 'Something went wrong',
     });
   }
-};
+}

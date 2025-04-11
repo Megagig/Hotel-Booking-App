2869850114ed8f720540092c4837a2b7293c49b1
megagig <megagigsoftwaresolutions@gmail.com>
2025-04-11 15:17:02 +0100

    Add Password Field with Validation

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -54,6 +54,23 @@ const Register = () => {
           <span className="text-red-500">{errors.email.message}</span>
         )}
       </label>
+      <label className="text-gray-700 text-sm font-bold">
+        Password
+        <input
+          type="password"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register('password', {
+            required: 'This field is required',
+            minLength: {
+              value: 6,
+              message: 'Password must be at least 6 characters',
+            },
+          })}
+        />
+        {errors.password && (
+          <span className="text-red-500">{errors.password.message}</span>
+        )}
+      </label>
     </form>
   );
 };

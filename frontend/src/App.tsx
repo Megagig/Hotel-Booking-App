372767d32acd09764566dd302de11edd6daa22a5
megagig <megagigsoftwaresolutions@gmail.com>
2025-04-11 14:49:40 +0100

    Add the Register Route to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import Layout from './layouts/layout';
+import Register from './pages/Register';
 
 const App = () => {
   return (
@@ -23,6 +29,15 @@ const App = () => {
             </Layout>
           }
         />
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );

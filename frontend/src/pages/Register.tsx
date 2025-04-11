ab793e865642ee2a23273b994da0e567cfe404a5
megagig <megagigsoftwaresolutions@gmail.com>
2025-04-11 15:12:34 +0100

    Install React Hook Form,Set Up Form Validation Types and Hooks,Add First Name and Last Name Fields,Fix Container Padding for Responsiveness

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,7 +1,48 @@
+import { useForm } from 'react-hook-form';
+
+// Define form data type
+type RegisterFormData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
 const Register = () => {
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormData>();
+
   return (
     <form className="flex flex-col gap-5">
       <h2 className="text-3xl font-bold">Create an account</h2>
+      <div className="flex flex-col md:flex-row gap-5">
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          First Name
+          <input
+            className="border rounded w-full py-1 px-2 font-normal"
+            {...register('firstName', { required: 'This field is required' })}
+          />
+          {errors.firstName && (
+            <span className="text-red-500">{errors.firstName.message}</span>
+          )}
+        </label>
+
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          Last Name
+          <input
+            className="border rounded w-full py-1 px-2 font-normal"
+            {...register('lastName', { required: 'This field is required' })}
+          />
+          {errors.lastName && (
+            <span className="text-red-500">{errors.lastName.message}</span>
+          )}
+        </label>
+      </div>
     </form>
   );
 };

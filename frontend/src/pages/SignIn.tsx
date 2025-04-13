c05c88f5dfc03f92945f88fe61b46aaa55b75786
megagig <megagigsoftwaresolutions@gmail.com>
2025-04-13 15:33:08 +0100

    Add onSubmit and button to the form:

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -32,7 +32,7 @@ const SignIn = () => {
   });
 
   return (
-    <form className="flex flex-col gap-5">
+    <form className="flex flex-col gap-5" onSubmit={onSubmit}>
       <h2 className="text-3xl font-bold">Sign In</h2>
 
       {/* Email field */}

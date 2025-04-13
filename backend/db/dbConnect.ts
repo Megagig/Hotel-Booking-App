f2421c6766e11c4972f2988e488628414db68956
megagig <megagigsoftwaresolutions@gmail.com>
2025-04-13 20:34:26 +0100

    Refactor the dbConnect file to connect to the e2e database

--- backend/db/dbConnect.ts
+++ backend/db/dbConnect.ts
@@ -2,8 +2,9 @@ import mongoose from 'mongoose';
 
 const connectDB = async (): Promise<void> => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
-    console.log('MongoDB connected!');
+    await mongoose
+      .connect(process.env.MONGODB_URI as string)
+      .then(() => console.log('MongoDB connected: ', process.env.MONGODB_URI));
   } catch (err) {
     console.error('MongoDB connection failed:', err);
     process.exit(1);

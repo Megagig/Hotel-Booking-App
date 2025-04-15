500f70efd29cb944080eb4902e37d2bb9665c0d8
megagig <megagigsoftwaresolutions@gmail.com>
2025-04-15 23:20:07 +0100

    Deployment

--- frontend/src/api-clients.ts
+++ frontend/src/api-clients.ts
@@ -1,7 +1,7 @@
 import { RegisterFormData } from './pages/Register';
 import { SignInFormData } from './pages/SignIn';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
 
 //Create the register API call: This function will send a POST request to the server to create a new user.
 export const register = async (formData: RegisterFormData) => {

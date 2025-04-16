eaadb50085c3c569a8ced7492c105da2e64367da
megagig <megagigsoftwaresolutions@gmail.com>
2025-04-16 10:13:28 +0100

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -36,6 +36,11 @@ app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
+// Handle frontend routes - Add this before your static file serving
+app.get('/*', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
+});
+
 //Start the server
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);

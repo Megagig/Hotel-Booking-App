b9d226e0842ab55666e5e5632c2b7ccf1c2a850a
megagig <megagigsoftwaresolutions@gmail.com>
2025-04-16 10:45:42 +0100

    Preparing deployment to render

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -31,13 +31,13 @@ app.use(
 );
 
 // Serve static files from the frontend build directory
-// app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
 // Handle frontend routes - Add this before your static file serving
-app.get('/*', (req: Request, res: Response) => {
+app.get('*', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
 });
 
